45da55c218ab50eb7437c2d511272a61965e206c
Aashif <mhdaashif743@gmail.com>
2024-12-30 15:41:57 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { footerLinks } from '../constants'
+
+const Footer = () => {
+  return (
+    <footer className="py-5 sm:px-10 px-5">
+      <div className="screen-max-width">
+      <div>
+        <p className="font-semibold text-gray text-xs">
+          To Develop a Website: {' '}
+          <span className="underline text-blue">
+            <a href="https://orbitalitsolutions.com" target="_blank" rel="noopener noreferrer">
+              Visit Orbital IT Solutions
+            </a>
+          </span>
+            {' '}or{' '}
+          <span className="underline text-blue">
+            <a href="https://orbitalitsolutions.com" target="_blank" rel="noopener noreferrer">
+              contact us
+            </a>
+          </span>{' '}
+            for more information.
+        </p>
+          <p className="font-semibold text-gray text-xs">
+            Or call +94 767 855 743
+          </p>
+      </div>
+
+
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-gray text-xs">Copright @ 2025 Apple Inc. All rights reserved.</p>
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{' '}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
